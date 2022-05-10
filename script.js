3c6089764e05f8270856e6c2915545c73332cfa3
bigmosi <kinyeramo@gmail.com>
2022-05-10 20:57:10 +0300

    Addded javascript code to script file

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ function menuOptions(param) {
   const links = document.getElementById('myNav');
   links.style.display = 'none';
 }
-
-  menuOptions();
+menuOptions();
 
 function closeNav() {
   document.getElementById('myNav').style.height = '0%';
