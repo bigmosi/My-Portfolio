d708c887f5150515a95af11c5638a889c6880a00
bigmosi <kinyeramo@gmail.com>
2022-05-10 11:55:20 +0300

    fixed linter errors

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
+    document.getElementById('myNav').style.height = '100%';
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
+      document.getElementById('myNav').style.height = '0%';
 }
 
-
-let btn = document.querySelector('#btn');
+const btn = document.querySelector('#btn');
 
 function display() {
     alert('It was click');
