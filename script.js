0049c1dad5d4e0874d9f356693a77fb61990e086
bigmosi <kinyeramo@gmail.com>
2022-05-22 00:02:56 +0300

    working on live button

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ let Data = [
     popuptext: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.',
     image: {
       image1: 'assets/images/pex.jpeg',
-      popup: 'assets/images/cdc-events.jpg',
+      popup: 'assets/images/pex1.jpeg',
     },
     technologies: {
       css: 'css',
