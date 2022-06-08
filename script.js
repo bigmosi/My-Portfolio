2c31f7b3a075ab68a02225d32d48e80f1206e270
bigmosi <kinyeramo@gmail.com>
2022-06-08 13:49:41 +0300

    Add live link to project

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ let Data = [
     description: '\n' + 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     popuptext: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.',
     image: {
-      image1: 'assets/images/pex.jpeg',
+      image1: 'assets/images/shoot1.png',
       popup: 'assets/images/pex1.jpeg',
     },
     technologies: {
