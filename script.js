a02540d3142571ef52b922d9783c56adeedd58f7
bigmosi <kinyeramo@gmail.com>
2022-07-23 17:48:00 +0300

    worked on styling the page

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ let Data = [
     popuptext: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.',
     image: {
       image1: 'assets/images/shoot1.png',
-      popup: 'assets/images/pex1.jpeg',
+      popup: 'assets/images/popimage.png',
     },
     technologies: {
       css: 'css',
@@ -131,7 +131,7 @@ let Data = [
     subtitle: 'Multi-Post Stories',
     description: '\n' + 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     image: {
-      image1: 'assets/images/pex2.jpeg',
+      image1: 'assets/images/popimage.png',
     },
     technologies: {
       css: 'css',
